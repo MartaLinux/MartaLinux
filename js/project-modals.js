8596f0ed811a2ae0977559ff549c46de63cf353f
MartaLinux <Akumakun507@gmail.com>
2022-04-15 19:16:29 +0300

    06 and iframe (css,js)

--- js/project-modals.js
+++ js/project-modals.js
@@ -3,19 +3,20 @@ const projectModalDragAndDrop = document.querySelector('#personal-project-drag-a
 const projectModaSliderTwo = document.querySelector('#personal-project-slider-two');
 const projectModalPixes = document.querySelector('#personal-project-pixes');
 const projectModalClickerGame = document.querySelector('#personal-project-clicker-game');
-
+const projectModalSpinnerAnimation = document.querySelector('#personal-project-spinner-animation');
 
 
 const projectOpenBtnSliderOne = document.querySelector('#personal-project-slider-one-btn');
 const projectOpenBtnDragAndDrop = document.querySelector('#personal-project-drag-and-drop-btn');
 const projectOpenBtnSliderTwo = document.querySelector('#personal-project-slider-two-btn');
 const projectOpenBtnPixes = document.querySelector('#personal-project-pixes-btn');
-const projectOpenBtnClickerGame = document.querySelector('#personal-project-clicker-game-btn')
+const projectOpenBtnClickerGame = document.querySelector('#personal-project-clicker-game-btn');
+const projectOpenBtnSpinnerAnimation = document.querySelector('#personal-project-spinner-animation-btn');
 
 
 
-const projectModals = [ projectModalSliderOne, projectModalDragAndDrop, projectModaSliderTwo, projectModalPixes, projectModalClickerGame];
-const projectBtns = [ projectOpenBtnSliderOne, projectOpenBtnDragAndDrop, projectOpenBtnSliderTwo, projectOpenBtnPixes, projectOpenBtnClickerGame];
+const projectModals = [ projectModalSliderOne, projectModalDragAndDrop, projectModaSliderTwo, projectModalPixes, projectModalClickerGame, projectModalSpinnerAnimation];
+const projectBtns = [ projectOpenBtnSliderOne, projectOpenBtnDragAndDrop, projectOpenBtnSliderTwo, projectOpenBtnPixes, projectOpenBtnClickerGame, projectOpenBtnSpinnerAnimation];
 
 projectBtns.forEach((btn, index) => {
     const projectModal = projectModals[index];
